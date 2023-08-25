11d118dae52db9fad3c4bb8d09ccc962e71b2857
Mahedi Sabuj <mahedi.sabuj@abbvie.com>
2023-08-25 20:13:41 +0600

    dispatcher configuration

--- aem-author/README.md
+++ aem-author/README.md
@@ -14,6 +14,6 @@
 - `docker push ghcr.io/mahedisabuj/aem-docker/aem-author:6.5.16` # Need write:packages Permission to Password Token
 
 ## Multi Platform Support
-- `docker buildx create aem-builder`
+- `docker buildx create --name aem-builder`
 - `docker buildx use aem-builder`
 - `docker buildx build --platform linux/amd64,linux/arm64 -t ghcr.io/mahedisabuj/aem-docker/aem-author:6.5.16 --push .`
--- aem-dispatcher/src/scripts/setup.sh
+++ aem-dispatcher/src/scripts/setup.sh
@@ -1,5 +1,11 @@
 #!/bin/bash
 
+DISPARCH=x86_64
+
+if [ "${TARGETARCH}" = "arm64" ]; then
+    DISPARCH=aarch64
+fi
+
 #create default docroots
 mkdir -p /mnt/var/www/html
 chown apache:apache /mnt/var/www/html
@@ -27,3 +33,12 @@ ln -s /etc/httpd/conf.dispatcher.d/available_farms/002_ams_author_farm.any /etc/
 ln -s /etc/httpd/conf.dispatcher.d/available_farms/002_ams_publish_farm.any /etc/httpd/conf.dispatcher.d/enabled_farms/002_ams_publish_farm.any
 
 #set up dispatcher
+mkdir -p /tmp/dispatcher
+
+curl -o /tmp/dispatcher/dispatcher.tar.gz https://download.macromedia.com/dispatcher/download/dispatcher-apache2.4-linux-$DISPARCH-4.3.5.tar.gz
+
+cd /tmp/dispatcher
+
+tar zxvf dispatcher.tar.gz
+
+cp -v dispatcher-apache2.4-4.3.5.so /etc/httpd/modules/mod_dispatcher.so
